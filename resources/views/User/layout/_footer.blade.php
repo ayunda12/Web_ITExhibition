ec1aa368861af9e4e44df914f607f3d06fcaa89a
indahayuningtias08 <80672080+indahayuningtias08@users.noreply.github.com>
2022-06-05 20:57:06 +0700

    Add files via upload

---
+++ resources/views/User/layout/_footer.blade.php
@@ -0,0 +1,80 @@
+<footer class="main-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-6">
+                <div class="footer-info">
+                    <h3>Gedung Jurusan Teknologi Informasi</h3>
+                    <p class="pb-3"><em>Politeknik Negeri Jember
+                            Jl. Mastrip PO.BOX 164 Jember,
+                            Jawa Timur 68101, Indonesia</em></p>
+
+                    <p>
+                        <strong>Phone:</strong> (+62) 331-333532<br>
+                        <strong>Email:</strong> jti1@polije.ac.id<br>
+                    </p>
+                    <ul class="social-icons list-inline">
+                        <li class="list-inline-item"><a href="#" target="_blank" title="Facebook"><i
+                                    class="fa fa-facebook"></i></a></li>
+                        <li class="list-inline-item"><a href="#" target="_blank" title="Twitter"><i
+                                    class="fa fa-twitter"></i></a></li>
+                        <li class="list-inline-item"><a href="#" target="_blank" title="Instagram"><i
+                                    class="fa fa-instagram"></i></a></li>
+                        <li class="list-inline-item"><a href="#" target="_blank" title="Pinterest"><i
+                                    class="fa fa-pinterest"></i></a></li>
+                    </ul>
+
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6">
+                <h5>Menu</h5>
+                <ul class="links list-unstyled">
+                    <li> <a href="#">Beranda</a></li>
+                    <li> <a href="#">Tentang</a></li>
+                    <li> <a href="#">Produk</a></li>
+                    <li> <a href="#">Testimoni</a></li>
+                    <li> <a href="#">Kontak</a></li>
+                </ul>
+            </div>
+            {{-- <div class="col-lg-3 col-md-6">
+                <h5>Selected Cases</h5>
+                <ul class="links list-unstyled">
+                    <li> <a href="#">Guess Conntect</a></li>
+                    <li> <a href="#">Postly</a></li>
+                    <li> <a href="#">Iris Vor Arnim</a></li>
+                    <li> <a href="#">Yapital</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <h5>Selected Cases</h5>
+                <ul class="links list-unstyled">
+                    <li> <a href="#">Guess Conntect</a></li>
+                    <li> <a href="#">Postly</a></li>
+                    <li> <a href="#">Iris Vor Arnim</a></li>
+                    <li> <a href="#">Yapital</a></li>
+                </ul>
+            </div> --}}
+        </div>
+    </div>
+    <div class="copyrights">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <p>&copy; IT Exhibition
+                        <script>
+                            document.write(new Date().getFullYear())
+                        </script>. All rights reserved.
+                    </p>
+                    <div class="credits">
+                        Made with <g-emoji class="g-emoji" alias="heart"
+                            fallback-src="https://github.githubassets.com/images/icons/emoji/unicode/2764.png">
+                            <img class="emoji" alt="heart" height="20" width="20"
+                                src="https://github.githubassets.com/images/icons/emoji/unicode/2764.png">
+                        </g-emoji>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</footer>
