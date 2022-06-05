06db29f37228e09082e4be5444cd27396a90c0d1
WidyaOctaviola <80468883+widya37@users.noreply.github.com>
2022-06-05 21:08:38 +0700

    Add files via upload

---
+++ resources/views/admin/navbar.blade.php
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl " id="navbarBlur"
+    data-scroll="false">
+    <div class="container-fluid py-1 px-3">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
+                <li class="breadcrumb-item text-sm"><a class="opacity-5 text-white" href="javascript:;">Pages</a></li>
+                <li class="breadcrumb-item text-sm text-white active" aria-current="page">Dashboard</li>
+            </ol>
+            <h6 class="font-weight-bolder text-white mb-0">@yield('page')</h6>
+        </nav>
+
+        <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
+
+            <ul class="navbar-nav  justify-content-end ms-auto">
+
+                <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
+                    <a href="javascript:;" class="nav-link text-white p-0" id="iconNavbarSidenav">
+                        <div class="sidenav-toggler-inner">
+                            <i class="sidenav-toggler-line bg-white"></i>
+                            <i class="sidenav-toggler-line bg-white"></i>
+                            <i class="sidenav-toggler-line bg-white"></i>
+                        </div>
+                    </a>
+                </li>
+                <li class="nav-item dropdown pe-2 d-flex align-items-center">
+                    <a href="" class="nav-link text-white p-0" id="dropdownMenuButton" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        <i class="fa fa-user me-sm-1"></i> <span
+                            class="d-sm-inline d-none">{{ auth()->user()->nama }}</span>
+                    </a>
+
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
