3bc3246b17545fa7493ce1ac3574a8d439b0fe25
indahayuningtias08 <80672080+indahayuningtias08@users.noreply.github.com>
2022-06-05 20:56:19 +0700

    Add files via upload

---
+++ resources/views/User/layout/_navbar.blade.php
@@ -0,0 +1,22 @@
+<header class="header">
+    <nav class="navbar navbar-expand-lg fixed-top">
+        <a href="index.html" class="navbar-brand"><img src="{{ asset('assetsuser/') }}/img/logo.png" width="25%"></a>
+        <button type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"
+            class="navbar-toggler navbar-toggler-right"><span></span><span></span><span></span></button>
+        <div id="navbarSupportedContent" class="collapse navbar-collapse">
+            <ul class="navbar-nav ml-auto align-items-start align-items-lg-center">
+                <li class="nav-item"><a href="#about-us" class="nav-link link-scroll">Beranda</a></li>
+                <li class="nav-item"><a href="#about-us" class="nav-link link-scroll">Tentang</a></li>
+                <li class="nav-item"><a href="#features" class="nav-link link-scroll">Produk</a></li>
+                <li class="nav-item"><a href="#testimonials" class="nav-link link-scroll">Testimoni</a>
+                </li>
+                <li class="nav-item"><a href="text.html" class="nav-link">Kontak</a></li>
+            </ul>
+            <div class="navbar-text">
+                <!-- Button trigger modal--><a href="{{ route('login') }}"
+                    class="btn btn-primary navbar-btn btn-shadow btn-gradient">Login</a>
+            </div>
+        </div>
+    </nav>
+</header>
